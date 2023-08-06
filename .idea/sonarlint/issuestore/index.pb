d0b41b71cc0846202a1b31e6beecead21e06a325
Rohan Choudhary <anandc.rohan484@gmail.com>
2023-08-06 17:43:19 +0530

    [add] wip working on Add Note Fragment UI

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+z
+Japp/src/main/java/com/example/note_z/ui/fragments/update/UpdateFragment.kt,d\c\dccbf52b7e3d155073b8f91001eee8646705ecae
+t
+Dapp/src/main/java/com/example/note_z/ui/fragments/add/AddFragment.kt,0\0\004892174f74017b7fb0fa8020965b269b2b199b
--- app/src/main/java/com/example/note_z/MainActivity.kt
+++ app/src/main/java/com/example/note_z/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.note_z
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.note_z.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
